bb50be49c46397fd76948d5c2ce725d82e7a1115
navtesh21 <makennavtesh@gmail.com>
2024-05-20 16:25:08 +0530

    errors fixed

--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,16 @@ import path from "path";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  build: {
+    rollupOptions: {
+      input: path.resolve(__dirname, "index.html"), // Ensure this points to the root index.html
     },
   },
 });
